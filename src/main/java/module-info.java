af9bb2a156f4398a44ce277ad177198cf194dbc2
af <fq@keevol.cn>
2025-12-14 14:41:37 +0800

    refix font issue

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.keenotes.mobile {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.graphics;
+    requires javafx.base;
     requires java.net.http;
     requires com.gluonhq.attach.display;
     requires com.gluonhq.attach.lifecycle;
@@ -9,5 +10,5 @@ module com.keenotes.mobile {
     requires com.gluonhq.attach.util;
 
     exports cn.keevol.keenotes.mobilefx;
-    opens cn.keevol.keenotes.mobilefx to javafx.fxml;
+    opens cn.keevol.keenotes.mobilefx to javafx.fxml, javafx.graphics, javafx.base;
 }
