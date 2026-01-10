c8322cc41672e8c2dc13b709161a9935ea6f1886
af <fq@keevol.cn>
2026-01-10 18:24:01 +0800

    change icons

--- src/main/java/cn/keevol/keenotes/mobilefx/SidebarView.java
+++ src/main/java/cn/keevol/keenotes/mobilefx/SidebarView.java
@@ -4,6 +4,7 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Priority;
+import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
@@ -119,39 +120,53 @@ public class SidebarView extends VBox {
     }
     
     /**
-     * Create note icon (document with pen)
+     * Create note icon from PNG
      */
     private javafx.scene.Node createNoteIcon() {
-        javafx.scene.shape.SVGPath icon = new javafx.scene.shape.SVGPath();
-        icon.setContent("M14 2H6C4.9 2 4 2.9 4 4V20C4 21.1 4.9 22 6 22H18C19.1 22 20 21.1 20 20V8L14 2M18 20H6V4H13V9H18V20M15 11H8V13H15V11M17 15H8V17H17V15");
-        return icon;
+        return createIconFromResource("/icons/take_note.png");
     }
     
     /**
-     * Create search icon (magnifying glass)
+     * Create search icon from PNG
      */
     private javafx.scene.Node createSearchIcon() {
-        javafx.scene.shape.SVGPath icon = new javafx.scene.shape.SVGPath();
-        icon.setContent("M15.5 14H14.71L14.43 13.73C15.41 12.59 16 11.11 16 9.5C16 5.91 13.09 3 9.5 3C5.91 3 3 5.91 3 9.5C3 13.09 5.91 16 9.5 16C11.11 16 12.59 15.41 13.73 14.43L14 14.71V15.5L19 20.49L20.49 19L15.5 14M9.5 14C7.01 14 5 11.99 5 9.5C5 7.01 7.01 5 9.5 5C11.99 5 14 7.01 14 9.5C14 11.99 11.99 14 9.5 14");
-        return icon;
+        return createIconFromResource("/icons/search.png");
     }
     
     /**
-     * Create review icon (book/library)
+     * Create review icon from PNG
      */
     private javafx.scene.Node createReviewIcon() {
-        javafx.scene.shape.SVGPath icon = new javafx.scene.shape.SVGPath();
-        icon.setContent("M18 2H12V9L9.5 7.5L7 9V2H6C4.9 2 4 2.9 4 4V20C4 21.1 4.9 22 6 22H18C19.1 22 20 21.1 20 20V4C20 2.9 19.1 2 18 2");
-        return icon;
+        return createIconFromResource("/icons/review.png");
     }
     
     /**
-     * Create settings icon (gear)
+     * Create settings icon from PNG
      */
     private javafx.scene.Node createSettingsIcon() {
-        javafx.scene.shape.SVGPath icon = new javafx.scene.shape.SVGPath();
-        icon.setContent("M12 15.5C10.07 15.5 8.5 13.93 8.5 12C8.5 10.07 10.07 8.5 12 8.5C13.93 8.5 15.5 10.07 15.5 12C15.5 13.93 13.93 15.5 12 15.5M19.43 12.97C19.47 12.65 19.5 12.33 19.5 12C19.5 11.67 19.47 11.34 19.43 11L21.54 9.37C21.73 9.22 21.78 8.95 21.66 8.73L19.66 5.27C19.54 5.05 19.27 4.96 19.05 5.05L16.56 6.05C16.04 5.66 15.5 5.32 14.87 5.07L14.5 2.42C14.46 2.18 14.25 2 14 2H10C9.75 2 9.54 2.18 9.5 2.42L9.13 5.07C8.5 5.32 7.96 5.66 7.44 6.05L4.95 5.05C4.73 4.96 4.46 5.05 4.34 5.27L2.34 8.73C2.21 8.95 2.27 9.22 2.46 9.37L4.57 11C4.53 11.34 4.5 11.67 4.5 12C4.5 12.33 4.53 12.65 4.57 12.97L2.46 14.63C2.27 14.78 2.21 15.05 2.34 15.27L4.34 18.73C4.46 18.95 4.73 19.03 4.95 18.95L7.44 17.94C7.96 18.34 8.5 18.68 9.13 18.93L9.5 21.58C9.54 21.82 9.75 22 10 22H14C14.25 22 14.46 21.82 14.5 21.58L14.87 18.93C15.5 18.67 16.04 18.34 16.56 17.94L19.05 18.95C19.27 19.03 19.54 18.95 19.66 18.73L21.66 15.27C21.78 15.05 21.73 14.78 21.54 14.63L19.43 12.97Z");
-        return icon;
+        return createIconFromResource("/icons/settings.png");
+    }
+    
+    /**
+     * Helper method to create ImageView from resource path
+     */
+    private javafx.scene.Node createIconFromResource(String resourcePath) {
+        try {
+            javafx.scene.image.Image image = new javafx.scene.image.Image(
+                getClass().getResourceAsStream(resourcePath)
+            );
+            javafx.scene.image.ImageView imageView = new javafx.scene.image.ImageView(image);
+            imageView.setFitWidth(20);
+            imageView.setFitHeight(20);
+            imageView.setPreserveRatio(true);
+            return imageView;
+        } catch (Exception e) {
+            System.err.println("Could not load icon: " + resourcePath + " - " + e.getMessage());
+            // Fallback to empty region
+            Region placeholder = new Region();
+            placeholder.setPrefSize(20, 20);
+            return placeholder;
+        }
     }
     
     /**
