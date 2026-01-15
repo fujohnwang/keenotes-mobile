f065512604e7c67a35cc19f421a19cebe0472da2
af <fq@keevol.cn>
2026-01-15 14:27:55 +0800

    add query entry back on android and polish spinner

--- keenotes-android/app/src/main/java/cn/keevol/keenotes/ui/note/NoteFragment.kt
+++ keenotes-android/app/src/main/java/cn/keevol/keenotes/ui/note/NoteFragment.kt
@@ -2,15 +2,10 @@ package cn.keevol.keenotes.ui.note
 
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.Menu
-import android.view.MenuInflater
-import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.view.MenuProvider
 import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import cn.keevol.keenotes.KeeNotesApp
@@ -39,7 +34,7 @@ class NoteFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         
-        setupToolbar()
+        setupSearchButton()
         setupOverviewCard()
         setupNoteInput()
         setupSendButton()
@@ -151,24 +146,10 @@ class NoteFragment : Fragment() {
         }
     }
     
-    private fun setupToolbar() {
-        // Add menu to toolbar
-        requireActivity().addMenuProvider(object : MenuProvider {
-            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
-                menu.clear()
-                menuInflater.inflate(R.menu.note_menu, menu)
-            }
-            
-            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
-                return when (menuItem.itemId) {
-                    R.id.action_search -> {
-                        findNavController().navigate(R.id.action_note_to_search)
-                        true
-                    }
-                    else -> false
-                }
-            }
-        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
+    private fun setupSearchButton() {
+        binding.btnSearch.setOnClickListener {
+            findNavController().navigate(R.id.action_note_to_search)
+        }
     }
     
     private fun setupNoteInput() {
